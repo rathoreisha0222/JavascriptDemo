1fae0e5cb2c10fbc3a902f6a05e1966c1af0f4fe
rathoreisha0222 <144460676+rathoreisha0222@users.noreply.github.com>
2023-09-09 13:40:29 +0000

    datatypes done

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,10 @@
+let age=10
+let name="ISha"
+account=10;
+account="name"
+let accountid
+names=null
+console.log(typeof (name))
+console.log(typeof(account))
+console.log(typeof(accountId))
+console.log(typeof(names))
