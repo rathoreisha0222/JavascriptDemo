d511a38480c12983ba4660b9efbd53bfc9e9865e
rathoreisha0222 <144460676+rathoreisha0222@users.noreply.github.com>
2023-09-09 13:35:29 +0000

    constants and variables

---
+++ 01_Basics/01_VariableConstants.js
@@ -0,0 +1,9 @@
+const accountId=101
+let accountName="ISha"
+var accountPassword=1234
+accountEmail="isha@gmail.com"
+//accountId=1234
+
+console.log(accountId)
+accountPassword=12345
+console.table([accountId,accountName,accountPassword,accountEmail])
