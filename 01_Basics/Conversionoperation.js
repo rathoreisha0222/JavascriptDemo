2f417d1201f76665a96af680b914ac3f3b7adee4
rathoreisha0222 <144460676+rathoreisha0222@users.noreply.github.com>
2023-09-09 14:02:05 +0000

    conversion operation done

---
+++ 01_Basics/Conversionoperation.js
@@ -0,0 +1,17 @@
+//let score="33"
+let score="33abc"
+console.log(typeof score)
+let isnumber=Number(score)
+console.log(isnumber)
+console.log(typeof isnumber)
+
+//let islogged=1
+let islogged=0
+let booleanlogged=Boolean(islogged)
+console.log(booleanlogged)
+
+let number1=10
+console.log(typeof number1)
+let isString=String(number1)
+console.log(isString)
+console.log(typeof isString)
