51c6e7a9309d2663cefc1eefa306d7d772f2d0b3
rathoreisha0222 <144460676+rathoreisha0222@users.noreply.github.com>
2023-09-11 06:05:03 +0000

    prefixpostfix

---
+++ 01_Basics/operation.js
@@ -0,0 +1,25 @@
+let str1="Hello"
+let str2="  Isha"
+console.log(str1+str2)
+console.log(2+2)
+console.log("2"+2)
+console.log(2+"2")
+console.log("1"+1+2)
+console.log(2+1+"1")
+console.log(true)
+console.log(+true)
+// prefix and post fix
+let gamecounter=1
+++gamecounter
+console.log(gamecounter)
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
