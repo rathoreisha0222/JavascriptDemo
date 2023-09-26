cf83c4dbf6dee950cdb437168420b96a0b77d997
rathoreisha0222 <144460676+rathoreisha0222@users.noreply.github.com>
2023-09-26 13:04:07 +0000

    stack and heap

---
+++ 01_Basics/StackandHeap.js
@@ -0,0 +1,15 @@
+let myname='isha'
+let myyoutubename=myname;
+myyoutubename='isharathore'
+console.log(myyoutubename)
+console.log(myname)
+
+let user1={
+    email:'isha$534',
+    pin:345
+}
+
+let user2=user1;
+user2.email='rathore345'
+console.log(user1)
+console.log(user2)
